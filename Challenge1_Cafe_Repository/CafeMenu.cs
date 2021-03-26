ee5144d76babb8c6c81001b5d2a926f4317d0f84
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-03-25 20:41:11 -0400

    added properties to CafeMenu class with Challenge 1 Repo

--- Challenge1_Cafe_Repository/CafeMenu.cs
+++ Challenge1_Cafe_Repository/CafeMenu.cs
@@ -9,5 +9,11 @@ namespace Challenge1_Cafe_Repository
 {
     public class CafeMenu
     {
+        //Required menu items: meal number, meal name, description, list of ingredients, price
+        //Properties: meal number, meal name, description, price
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
