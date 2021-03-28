f62495c15e8470e6d03dafb8737f45d5c66a6875
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-03-27 20:05:07 -0400

    setup test methods for repository methods for challenge 1

---
+++ Challenge1_Cafe_UnitTest/CafeMenu_RepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Challenge1_Cafe_UnitTest
+{
+    [TestClass]
+    public class CafeMenu_RepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod_ReturnList()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod_AddMenuItem()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod_ReadMenuItems()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod_DeleteMenuItem()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod_GetMenuItemByMealNumber()
+        {
+
+        }
+    }
+}
