4cfb73a30059644e2e5e960e4523f82ae8462a7a
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-03-28 20:05:26 -0400

    created outline for methods in UI

--- Challenge1_Cafe_Console/Program UI.cs
+++ Challenge1_Cafe_Console/Program UI.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Challenge1_Cafe_Repository;
 
 namespace Challenge1_Cafe_Console
 {
     class Program_UI
     {
+        private CafeMenu_Repository _cafeRepo = new CafeMenu_Repository();
+
+        //Run Method to start and run program
+        public void Run()
+        {
+        
+        }
+
+        //Menu method
+        private void Menu()
+        {
+
+        }
+
+        //Create a cafe menu item
+
+        //Read current cafe menu
+        
+        //Delete a cafe menu item
+
+
+
+        //SeedContentList to prevent starting with an empty list of CafeMenu
+        private void SeedContentList()
+        {
+
+        }
     }
 }
