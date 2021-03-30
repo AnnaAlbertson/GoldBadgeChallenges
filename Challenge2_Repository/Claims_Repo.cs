1f71ab957d383faed35b1ab7b4d9061a0251d0da
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-03-30 08:09:40 -0400

    scaffold claims repo for challenge 2

--- Challenge2_Repository/Claims_Repo.cs
+++ Challenge2_Repository/Claims_Repo.cs
@@ -8,18 +8,24 @@ namespace Challenge2_Repository
 {
     class Claims_Repo
     {
+        List<Claims> _listOfClaims = new List<Claims>();
+
+        //Retrieve list of claims to use in displaying the claims
         public void GetClaimsList()
         {
 
         }
+        //Update to assist in the queue option in UI
         public void UpdateClaims()
         {
 
         }
+        //To remove a claim once it has been completed in the queue in UI
         public void DeleteClaims()
         {
 
         }
+        //To create claim 
         public void CreateClaim()
         {
 
