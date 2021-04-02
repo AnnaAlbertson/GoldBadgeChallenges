45c7ea8fedccdfc0cd54a93134504f22dd522806
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-02 08:10:50 -0400

    Scaffolded out repo methods in challenge 3 repo

---
+++ Challenge3_Repository/Badges_Repo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3_Repository
+{
+    class Badges_Repo
+    {
+        private Dictionary<string, List<string>> badgesDictiionary = new Dictionary<string, List<string>>();
+        public void CreateBadge()
+        {
+
+        }
+        public void UpdateBadge()
+        {
+
+        }
+        public void LookUpBadgeByID()
+        {
+
+        }
+        public void AddBadgeIDKey()
+        {
+        }
+        public void AddDoorAccessValues()
+        {
+        }
+        public void GetAllBadges()
+        {
+
+        }
+    }
+}
