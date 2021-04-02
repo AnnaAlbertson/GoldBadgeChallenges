860f1907eacf33a7028de9c5027373e74e17a579
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-02 08:00:05 -0400

    scaffolded out methods in UI for challenge 3

--- Challenge3_Console/ProgramUI.cs
+++ Challenge3_Console/ProgramUI.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace Challenge3_Console
 {
     class ProgramUI
-    {
+    { // Run method containing methods in UI
+        public void Run()
+        {
+            Menu();
+        }
+        public void Menu()
+        {
+
+        }
+        
     }
 }
