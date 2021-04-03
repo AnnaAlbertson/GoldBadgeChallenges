a439c5be628141a4b66b3b2d5a45b7ae548022a5
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-03 16:23:08 -0400

    built program class for challenge 4

--- Challenge4_Console/Program.cs
+++ Challenge4_Console/Program.cs
@@ -10,6 +10,8 @@ namespace Challenge4_Console
     {
         static void Main(string[] args)
         {
+            ProgramUI program = new ProgramUI();
+            program.Run();
         }
     }
 }
