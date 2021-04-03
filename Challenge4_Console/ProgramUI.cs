fedae0884e9f00e2d35ecc92e633caf8cf4e9a0a
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-03 16:39:29 -0400

    built menu in ui for challenge 4

--- Challenge4_Console/ProgramUI.cs
+++ Challenge4_Console/ProgramUI.cs
@@ -14,7 +14,50 @@ namespace Challenge4_Console
         }
         private void Menu()
         {
+            //While Loop to keep program running
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                //Display options to user
+                Console.WriteLine("Welcome to Komodo Accounting Resource!\n" +
+                    "How would you like to continue?\n" +
+                    "\n" +                    
+                    "1) Display a list of all outings.\n" +
+                    "2) Add an outing.\n" +
+                    "3) View calculations.\n" +
+                    "4) Exit.\n" +
+                    "\n" +
+                    "Please enter your selection below then press enter to continue...");
 
+                string selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "1":
+                        // Display list of all outings
+                        DisplayOutings();
+                        break;
+                    case "2":
+                        // Add an outing
+                        AddOuting();            
+                        break;
+                    case "3":
+                        // View calculations
+                        Calculations();
+                        break;
+                    case "4":
+                        // Exit
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a number listed above.");
+                        break;
+                }
+
+                Console.WriteLine("Please press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
         private void DisplayOutings()
         {
@@ -23,6 +66,13 @@ namespace Challenge4_Console
         private void AddOuting()
         {
 
+        }
+        private void Calculations()
+        {
+            Console.WriteLine("Would you like to view...\n" +
+                "1) the combined cost of all outings\n" +
+                "or" +
+                "2) the cost of outings by type");
         }
         private void CombinedCostOutings()
         {
