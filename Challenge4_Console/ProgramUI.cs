f4a41c38fd5ecc402cc369c7a1ac6a86de8a39a0
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-03 16:22:04 -0400

    started challenge 4

---
+++ Challenge4_Console/ProgramUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge4_Console
+{
+    class ProgramUI
+    {
+        public void Run()
+        {
+            Menu();
+        }
+        private void Menu()
+        {
+
+        }
+        private void DisplayOutings()
+        {
+
+        }
+        private void AddOuting()
+        {
+
+        }
+        private void CombinedCostOutings()
+        {
+
+        }
+        private void CombinedCostByCategory()
+        {
+
+        }
+    }
+}
---
+++ Challenge4_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge4_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Challenge4_UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8be665e6-4cbc-48a2-9a6a-b2327dc4f2d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
