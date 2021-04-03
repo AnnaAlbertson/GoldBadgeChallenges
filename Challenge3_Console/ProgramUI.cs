6203352df647b1064026abf2bfb10be8514728aa
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-03 15:04:28 -0400

    built out menu cw for challenge 3

--- Challenge3_Console/ProgramUI.cs
+++ Challenge3_Console/ProgramUI.cs
@@ -15,7 +15,49 @@ namespace Challenge3_Console
         }
         private void Menu()
         {
+            Console.Clear();
+            //While Loop to keep program running
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                //Display options to user
+                Console.WriteLine("Hello Security Admin, What would you like to do?\n" +
+                    "\n" +
+                    "1) Add a badge.\n" +
+                    "2) Edit a badge.\n" +
+                    "3) List all badges.\n" +
+                    "4) Exit\n" +
+                    "\n" +
+                    "Please enter your selection below then press enter to continue...");
 
+                string selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "1":
+                        // Add a badge
+                        
+                        break;
+                    case "2":
+                        // Edit a badge
+                        
+                        break;
+                    case "3":
+                        // List all badges
+                        
+                        break;
+                    case "4":
+                        // leaving
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a menu item listed above.");
+                        break;
+                }
+                Console.WriteLine("\nPlease press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
         private void AddBadge()
         {
