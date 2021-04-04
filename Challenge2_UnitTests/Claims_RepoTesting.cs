d9c115253cb9e9cfa2b40639c620b19f9dca9eb6
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-04-03 22:17:15 -0400

    built out a test method for challenge 2

--- Challenge2_UnitTests/Claims_RepoTesting.cs
+++ Challenge2_UnitTests/Claims_RepoTesting.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Challenge2_Repository;
+using System.Collections.Generic;
 
 namespace Challenge2_UnitTests
 {
     [TestClass]
     public class Claims_RepoTesting
     {
+        private Claims_Repo repo = new Claims_Repo();
+        
         [TestMethod]
         public void Test_GetClaimsList()
         {
+            //Arrange
+            SeedClaimQueue();
+            //Act
+            Queue<Claims> localList = repo.GetClaimsQueue();
+            //Assert
+            Assert.AreEqual(3, localList.Count);
         }
         [TestMethod]
         public void Test_UpdateClaims()
@@ -22,6 +32,18 @@ namespace Challenge2_UnitTests
         public void Test_CreateClaim()
         {
 
+        }
+        private void SeedClaimQueue()
+        {
+            // Seeding Queue
+            Claims claim1 = new Claims(1, ClaimType.Car, "Car accident on 465.", 400.00m, new DateTime(2018, 04, 25), new DateTime(2018, 04, 11));
+            Claims claim2 = new Claims(2, ClaimType.Home, "House fire in kitchen.", 4000.00m, new DateTime(2018, 04, 11), new DateTime(2018, 04, 12));
+            Claims claim3 = new Claims(3, ClaimType.Theft, "Stolen pancakes.", 4.00m, new DateTime(2018, 04, 27), new DateTime(2018, 06, 1));
+
+            // Adding to Queue
+            repo.EnquequeClaim(claim1);
+            repo.EnquequeClaim(claim2);
+            repo.EnquequeClaim(claim3);
         }
     }
 }
